1eb87e621ea9d51a36e3451d9633836309f5649a
Daniel Mituku <dberhe490@gmail.com>
2024-03-08 03:59:59 +0000

    New files added

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-#Condtional satatemnet 
-'''n = int(input("Enter the number: "))
+#Condtional satatemnet
+
+"""n = int(input("Enter the number: "))
 
 if n > 0:
     print("n is a postive number")
 elif n<0:
     print("n is a negative number")
 else:
-    print("n is a zero")'''
+    print("n is a zero")"""
 
 
 #loops
@@ -72,3 +73,41 @@ for person in persons:
         print(f"We added {person} to flight successfully")
     else:
         print(f"No avalible seat for the {person}")"""
+
+#Data stucture using Python
+
+# Arrays:- we only have list on the python that is not static array
+
+"""list1 = ["Daniel", "Berhe", "Mituku"]
+
+print(list1[0])
+for i in range(len(list1)):
+    if list1[i] == "Mituku":
+        print(f"{list1[i]} has been found in here")
+    else:
+        print("We can not find the name")"""
+
+#Exercise
+
+"""myList = [ 2200, 2350,2600, 2130, 2190]
+
+print(myList[0]-myList[1])
+
+print(myList[0]+ myList[1] + myList[2])
+
+for i in myList:
+    if i == 2000:
+        print("Here we have got the 2000")
+
+myList.append(1980)
+print(myList)"""
+
+input = int(input("insert the max number"))
+list1 = []
+
+for i in range(input + 1):
+    if i % 2 != 0:
+        list1.append(i)
+
+print (list1)
+        
